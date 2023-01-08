814c9290ec78ee6498b65fd641b52679baeabd74
ArtInLines <linesinart@gmail.com>
2023-01-08 18:40:05 +0100

    Started adding test for rational

---
+++ java/polydiv/src/Test.java
@@ -0,0 +1,9 @@
+package polydiv.src;
+
+public class Test {
+	public static void main(String[] args) {
+		Rational r1 = new Rational(3);
+		Rational r2 = new Rational(1, 2);
+		System.out.println(r1.mul(r2));
+	}
+}
